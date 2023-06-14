43b9891ab3b6de199c899d4cc356cd1473ba8ed5
Manvitha-Bhat <manvitha.20cs043@sode-edu.in>
2023-06-14 12:43:46 +0530

    jk

--- pages/marks.php
+++ pages/marks.php
@@ -21,6 +21,7 @@
               <div class="table-responsive p-0">
                 <table class="table align-items-center mb-0">
                   <thead>
+                    <!-- hii -->
                     <tr>
                       <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Author</th>
                       <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Function</th>
