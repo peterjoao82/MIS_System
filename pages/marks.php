1e4abfc5080696fd873f650b68469618797e7d81
Missbahh <missbahbanu1@gmail.com>
2023-06-21 23:01:29 +0530

    Merge branch 'master' of github.com:Missbahh/MIS_System

--- pages/marks.php
+++ pages/marks.php
@@ -28,12 +28,14 @@
             </div>
             <div class="card-body px-0 pt-0 pb-2">
               <div class="table-responsive p-0">
+                <br></br>
                 <table class="table align-items-center justify-content-center mb-0">
                   <thead>
                     <tr>
                       <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2" text-align="center" >SUB CODE</th>
                       <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2" text-align="center">SUB NAME</th>
-                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2" text-align="center">AVERAGE</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2" text-align="center">SEMESTER</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2" text-align="center">AVERAGE MARKS</th>
                       <th></th>
                      
                     </tr>
@@ -44,13 +46,14 @@
                             $obtain_marks=0;
             
                              //  $roll_no=$_SESSION['user_id'];
-                                $query="SELECT m.ST_NO,m.SUB_CODE ,s.SUB_NAME, AVG(m.IA_1+m.IA_2+m.IA_3) as mm FROM marks m, subject s WHERE m.SUB_CODE=s.SUB_CODE AND m.ST_NO='4MW20CS001' GROUP by s.SUB_CODE";
+                                $query="SELECT m.ST_NO,m.SUB_CODE ,s.SUB_NAME,s.SEMESTER, AVG(m.IA_1+m.IA_2+m.IA_3) as mm FROM marks m, subject s WHERE m.SUB_CODE=s.SUB_CODE AND m.ST_NO='4MW20CS001' GROUP by s.SUB_CODE";
                                 $run=mysqli_query($conn,$query);
                                 while ($row=mysqli_fetch_array($run)) { 
                                   ?>
                                     <tr >
                                         <td><?php echo $row['SUB_CODE'] ?></td>
-                                        <td><?php echo $row['SUB_NAME'] ?></td>                                       
+                                        <td><?php echo $row['SUB_NAME'] ?></td>  
+                                        <td><?php echo $row['SEMESTER'] ?></td>       
                                         <td><?php echo $row['mm'] ?></td>
                                 
                         
