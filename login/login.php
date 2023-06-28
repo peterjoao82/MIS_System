50655d3bc83ba745358c07c9d8797b5ea29dd0e8
aaronsharon <aaronsharon_@hotmail.com>
2023-06-28 21:10:40 +0530

    Changes in LOGIN pages

--- login/login.php
+++ login/login.php
@@ -11,10 +11,9 @@
   <nav class="navbar navbar-expand-lg position-absolute top-0 z-index-3 w-100 shadow-none my-3 navbar-transparent mt-4">
     <div class="container">
       <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 text-white" href="../index.html">
-        Welcome to SMVITM MIS
+        <!-- <img src="/MIS_System/assets/img/smvitmlo" alt="smvitm" > -->
+        <img src="../assets/img/smvitmlo.jpg" alt="smvitm" style="width: 5em; border-radius: 5% ">
       </a>
-      
-          
       </div>
     </div>
   </nav>
--- pages/profile.php
+++ pages/profile.php
@@ -21,6 +21,11 @@
  ?>
 
 <!-- okay dd -->
+<style>
+  #btm1 {
+    text-decoration: underline;
+  }
+</style>
 <body class="g-sidenav-show bg-gray-100">
 
  
@@ -169,14 +174,13 @@
 								$query="SELECT DISTINCT sm.ST_NO as USN, sm.FNAME as Fname,sm.BR_CODE as bcode,sm.MOBILE_NO as mb,sm.StCollegeEMail as email, PASSWD as pass FROM studentmaster sm, employee e WHERE sm.ST_NO='$user_id'";
 								$run=mysqli_query($conn,$query);
 								while ($row=mysqli_fetch_array($run)) {?>
-							<div class="row">
-								<div class=" col-lg-6 col-md-6 pr-5">
+							<div class="row">					
+              <div class="col-md-6 pr-5">
 									<div class="form-group">
-										<label for="exampleInputEmail1">Name:</label>
-										<input type="text"  class="form-control" name="first_name" value=<?php echo $row['Fname']?> readonly>
+										<label for="exampleInputPassword1">Name</label>
+										<input type="text" class="form-control" name="course_code"  value="<?php echo $row['Fname'] ?>" readonly>
 									</div>
 								</div>
-								
 								<div class="col-md-6 pr-5">
 									<div class="form-group">
 										<label for="exampleInputPassword1">USN</label>
@@ -207,7 +211,7 @@
 								</div> -->
 								<div class="col-md-6 pr-5">
 									<div class="form-group">
-										<label for="exampleInputPassword1">Semester</label>
+										<label for="exampleInputPassword1">Email</label>
 										<input type="text" name="semester" class="form-control"  placeholder="Semester" value=<?php echo $row['email'] ?> readonly>
 									</div>
 								</div>
@@ -220,6 +224,7 @@
 							</div>
 							<?php } ?>
 						</form>
+            <p>Any Suggestions? <br> <a id="btm1" href="https://contact.oneshorts.in/"> Contact us.</a></p>
 					</div>
 				</div>	
                 <!-- <li class="nav-item">
