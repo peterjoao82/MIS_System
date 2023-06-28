54d2b6075b8419a6f858a6af2eb88cb49f683bdf
peterjoao82 <peterjoao82@gmail.com>
2023-06-28 21:15:46 +0530

    semi

--- pages/marks.php
+++ pages/marks.php
@@ -7,6 +7,7 @@ require_once "../connection/connection.php";
 
 ?>
 <?php
+$semi='1';
 if (isset($_POST['sub1'])) {
   $semi = $_POST['sem'];
 }
@@ -38,16 +39,14 @@ if (isset($_POST['viewdetails'])) {
           <div class="card mb-4">
             <div class="card-header pb-0">
               <h6>Marks - </h6>
-            </div>
-            <!-- <button name="viewdetails" type="submit"><a href="detailmarks.php">View Details</a></button> -->
-            
+            </div>            
             <form action="marks.php" method="POST">
               <div class="text-middle-align">
                 <div class="dropdown" style="text-align: right; margin-right:20px;">
                   <select class="browser-default custom-select" style="background-color:purple;color:white" name="sem">
-
+                 
                     <?php
-
+                  
                     $query = "select distinct SEMESTER as sem from subject ORDER BY SEMESTER";
                     $run = mysqli_query($conn, $query);
                     while ($row = mysqli_fetch_array($run)) {
@@ -79,9 +78,7 @@ if (isset($_POST['viewdetails'])) {
                   </thead>
                   <tbody>
                     <?php
-
-              
-
+                   
                     $roll_no = $_SESSION['user_id'];
                     $query = "SELECT m.ST_NO,m.SUB_CODE ,s.SUB_NAME,s.SEMESTER, AVG(m.IA_1+m.IA_2+m.IA_3) as mm FROM marks m, subject s WHERE m.SUB_CODE=s.SUB_CODE AND m.ST_NO='$roll_no' AND s.SEMESTER='$semi' GROUP by s.SUB_CODE";
                     $run = mysqli_query($conn, $query);
